4f9b1483a2fb81109baa0bafd0a8013aa88b63f2
amudao <amudao@rpi.edu>
2022-03-28 20:54:07 -0400

    Dynamic Website Using JSON JavaScript and Ajax

---
+++ IIT Lab9- Dynamic website/jsonData.js
@@ -0,0 +1,68 @@
+{
+    "menuItems": [
+        {
+            "menuName": "Lab3 (Resume)",
+            "menuDesc": "Here lies my first checkpoint. In here is my resume from high school. This lab is my starting point and my benchmark for me to beat and get better from.",
+            "menuURL": [
+                {
+                    "name":"Resume",
+                    "url":"Resources/Resume/resume.html"
+                }
+            ]
+        },
+        {
+            "menuName": "Lab4 (NewsFeed)",
+            "menuDesc": "In this segment of my website, I linked some of my favourite articles that I enjoyed reading. They range in topic, some covering anime, video games and the bussiness side of the gaming industry. I also linked an article to one of my favourite game developer (Housemarque) a Finnish studio that developed on of my favourite games of this generation (Returnal)",
+            "menuURL": [
+                {
+                    "name":"Atom Files",
+                    "url":"NewsFeed/atom.xml"
+                },
+                {
+                    "name":"RSS Files",
+                    "url":"NewsFeed/rss.rss"
+                }
+            ]
+        },
+        {
+            "menuName": "Lab5 (Interactive Form)",
+            "menuDesc": "In this lab we worked on creating an interactive form using javascript and html. The basics of this lab was to strengthen our skills in Javascript and HTML.",
+            "menuURL": [
+                {
+                    "name":"Interactive Form",
+                    "url":"./Interactive_Form/lab5.html"
+                }
+            ]
+        },
+        {
+            "menuName": "Lab6 (JQuery Form)",
+            "menuDesc": "In this lab we worked on creating an interactive form using javascript and html. We edited part of the using Javascript and JQuery",
+            "menuURL": [
+                {
+                    "name":"JQuery Form",
+                    "url":"./JQuery/lab6.html"
+                }
+            ]
+        },
+        {
+            "menuName": "Lab7 (XAMP)",
+            "menuDesc": "Understanding the basics of XAMP and how to use it for future uses",
+            "menuURL": [
+                {
+                    "name":"XAMP Form",
+                    "url":"./XAMP/lab8/lab8.html"
+                }
+            ]
+        },
+        {
+            "menuName": "Lab9 (Dynamic Website)",
+            "menuDesc": "Using JSON, JavaScript to write an JAx file to build a dynamic website that can be loaded without issues.",
+            "menuURL": [
+                {
+                    "name":"Dynamic website",
+                    "url":"./DynamicWebsite/project.html"
+                }
+            ]
+        }
+    ]
+}
